3f34472b228dc039be811d675b84016019c422e7
Michal Šesták <51904616+lertcz@users.noreply.github.com>
2020-10-26 16:05:59 +0100

    Update 2019_A:Pocty.py

--- 2019_A:Pocty.py
+++ 2019_A:Pocty.py
@@ -4,32 +4,22 @@ def log(a):
     if(DEBUG):
         print(a)
 
-T = int(input("T: "))
+T = int(input()) #T
 answers = []
 for t in range(T):
-    N = int(input("N: "))
-    print("Include spaces")
-    numRow = input("Numbers: ")
-    numRow.split()
-    arr = []
+    N = int(input()) #N
+    numRow = input() #numbers
+    numRow = numRow.split()
     correct = True
+        
+    for x in range(N):
+        if(int(numRow[x]) != x + 1 and int(numRow[x]) != 0):
+            correct = False
 
-    for x in range(0, len(numRow), 2):
-        arr.append(numRow[x])
-    
-    if(len(arr) == N):
-        for x in range(N):
-            if(int(arr[x]) != x + 1 and int(arr[x]) != 0):
-                correct = False
-            
-            log("debug: " + str(x + 1) + " " + arr[x])
-
-        if(correct):
-            answers.append("ANO")
-        else:
-            answers.append("NE")
+    if(correct):
+        answers.append("ANO")
     else:
-        print("\nN dont equal to number count")
+        answers.append("NE")
 
 for x in range(len(answers)):
     print(answers[x])
