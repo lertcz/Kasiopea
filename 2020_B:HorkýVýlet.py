413803cf5a5f3002ffdcb3c6e8ebd8c16a7de530
Michal Šesták <51904616+lertcz@users.noreply.github.com>
2020-11-21 19:11:06 +0100

    2020_B:HorkýVýlet.py

--- 2020_B:HorkýVýlet.py
+++ 2020_B:HorkýVýlet.py
@@ -6,17 +6,11 @@ def log(a):
 
 T = int(input()) #T
 answers = []
-highAnswer = 0
 for t in range(T):
     N = input()
     numRow = input() #days
     numRow = numRow.split()
 
-    #20 7 13 19 5 | 19
-    #18 40 3 | 40
-    #10 10 -5 10 -9 11 12 9 9 7 10 | 9 9 7 (9, nejvyssi teplota)
-    #
-    #1 3 -5 6 10
     #-------------------------------------
     high = []
     for x in range(int(N)-2):
